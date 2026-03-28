44ba89568dea2ffa0a7e6e989066679b82be84a3
Jason Aubrey <aubreyja@gmail.com>
2026-03-28 09:49:08 -0700

    Finished 3.2

--- my_files/C03_Logic/S02_The_Existential_Quantifier.lean
+++ my_files/C03_Logic/S02_The_Existential_Quantifier.lean
@@ -51,11 +51,30 @@ example (ubf : FnHasUb f) (ubg : FnHasUb g) : FnHasUb fun x ↦ f x + g x := by
   use a + b
   apply fnUb_add ubfa ubgb
 
+theorem fnLb_add {a b : ℝ} (hfa : FnLb f a) (hgb : FnLb g b) : FnLb (fun x ↦ f x + g x) (a + b) := by
+  intro x
+  dsimp
+  apply add_le_add
+  apply hfa
+  apply hgb
+
 example (lbf : FnHasLb f) (lbg : FnHasLb g) : FnHasLb fun x ↦ f x + g x := by
-  sorry
+  rcases lbf with ⟨a, lbfa⟩
+  rcases lbg with ⟨b, lbgb⟩
+  use a + b
+  apply fnLb_add lbfa lbgb
+
+
+#print mul_le_mul_of_nonneg_left
 
 example {c : ℝ} (ubf : FnHasUb f) (h : c ≥ 0) : FnHasUb fun x ↦ c * f x := by
-  sorry
+  rcases ubf with ⟨a,ubfa⟩
+  use c * a
+  intro x
+  dsimp
+  apply mul_le_mul_of_nonneg_left
+  apply ubfa
+  apply h
 
 example : FnHasUb f → FnHasUb g → FnHasUb fun x ↦ f x + g x := by
   rintro ⟨a, ubfa⟩ ⟨b, ubgb⟩
@@ -128,8 +147,21 @@ example (divab : a ∣ b) (divbc : b ∣ c) : a ∣ c := by
   rw [ceq, beq]
   use d * e; ring
 
+example (divab : a ∣ b) (divbc : b ∣ c) : a ∣ c := by
+  rcases divab with ⟨d,rfl⟩
+  rcases divbc with ⟨e,rfl⟩
+  use d * e; ring
+
 example (divab : a ∣ b) (divac : a ∣ c) : a ∣ b + c := by
-  sorry
+  rcases divab with ⟨d, beq⟩
+  rcases divac with ⟨e, ceq⟩
+  rw [beq,ceq]
+  use d + e; ring
+
+example (divab : a ∣ b) (divac : a ∣ c) : a ∣ b + c := by
+  rcases divab with ⟨d, rfl⟩
+  rcases divac with ⟨e, rfl⟩
+  use d + e; ring
 
 end
 
@@ -142,8 +174,14 @@ example {c : ℝ} : Surjective fun x ↦ x + c := by
   use x - c
   dsimp; ring
 
+#print mul_div_cancel₀
+
 example {c : ℝ} (h : c ≠ 0) : Surjective fun x ↦ c * x := by
-  sorry
+  intro x
+  use x / c
+  dsimp
+  apply mul_div_cancel₀
+  apply h
 
 example (x y : ℝ) (h : x - y ≠ 0) : (x ^ 2 - y ^ 2) / (x - y) = x + y := by
   field_simp [h]
@@ -163,6 +201,17 @@ variable {α : Type*} {β : Type*} {γ : Type*}
 variable {g : β → γ} {f : α → β}
 
 example (surjg : Surjective g) (surjf : Surjective f) : Surjective fun x ↦ g (f x) := by
-  sorry
+  intro z
+  dsimp
+  rcases surjg z with ⟨y,hz⟩
+  rcases surjf y with ⟨x,hx⟩
+  use x
+  rw[hx,hz]
 
+example (surjg : Surjective g) (surjf : Surjective f) : Surjective fun x ↦ g (f x) := by
+  intro z
+  dsimp
+  rcases surjg z with ⟨y,rfl⟩
+  rcases surjf y with ⟨x,rfl⟩
+  use x
 end
