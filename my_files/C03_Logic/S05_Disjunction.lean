932b2ef6c0990221b9251d8222496b2e310ede5b
Jason Aubrey <aubreyja@gmail.com>
2026-04-01 13:56:14 -0700

    Finished 3.5. I might be getting the hang of this!

--- my_files/C03_Logic/S05_Disjunction.lean
+++ my_files/C03_Logic/S05_Disjunction.lean
@@ -94,9 +94,34 @@ theorem lt_abs : x < |y| ↔ x < y ∨ x < -y := by
     linarith [abs_of_neg h]
   · intro h₂
     rcases le_or_gt 0 y with h | h
+    · rw [abs_of_nonneg h]
+      rcases h₂ with hxy | hxny
+      · exact hxy
+      · linarith
+    · rw [abs_of_neg h]
+      rcases h₂ with hxy | hxny
+      · linarith
+      · exact hxny
 
 theorem abs_lt : |x| < y ↔ -y < x ∧ x < y := by
-  sorry
+  constructor
+  rcases le_or_gt 0 x with h | h
+  · rw [abs_of_nonneg h]
+    intro h₁
+    constructor
+    linarith
+    assumption
+  · rw [abs_of_neg h]
+    intro h₁
+    constructor
+    linarith
+    linarith
+  rintro ⟨h₁,h₂⟩
+  rcases le_or_gt 0 x with h | h
+  · rw [abs_of_nonneg h]
+    assumption
+  · rw [abs_of_neg h]
+    linarith
 
 end MyAbs
 
@@ -117,23 +142,97 @@ example {m n k : ℕ} (h : m ∣ n ∨ m ∣ k) : m ∣ n * k := by
     apply dvd_mul_right
 
 example {z : ℝ} (h : ∃ x y, z = x ^ 2 + y ^ 2 ∨ z = x ^ 2 + y ^ 2 + 1) : z ≥ 0 := by
-  sorry
+  rcases h with ⟨a,b,h₁ | h₂⟩; linarith[pow_two_nonneg a,pow_two_nonneg b]; linarith[pow_two_nonneg a,pow_two_nonneg b]
+
+#check eq_zero_or_eq_zero_of_mul_eq_zero
 
 example {x : ℝ} (h : x ^ 2 = 1) : x = 1 ∨ x = -1 := by
-  sorry
+  have g: (x-1) * (x+1) =0 := by
+    calc
+    (x-1) * (x+1) = x^2 - 1 := by ring
+    _= 1 - 1 := by rw[h]
+    _= 0 := by norm_num
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero g with h₁ | h₂
+  left
+  linarith[h₁]
+  right
+  linarith[h₂]
 
 example {x y : ℝ} (h : x ^ 2 = y ^ 2) : x = y ∨ x = -y := by
-  sorry
+  have g : (x-y)*(x+y) = 0 := by
+    calc
+    (x-y)*(x+y) = x^2 - y^2 := by ring
+    _= x^2 - x^2 := by rw[h]
+    _= 0 := by apply sub_self
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero g with h₁ | h₂
+  left
+  linarith[h₁]
+  right
+  linarith[h₂]
 
 section
 variable {R : Type*} [CommRing R] [IsDomain R]
 variable (x y : R)
 
 example (h : x ^ 2 = 1) : x = 1 ∨ x = -1 := by
-  sorry
+  have g: (x-1) * (x+1) =0 := by
+    calc
+    (x-1) * (x+1) = x^2 - 1 := by ring
+    _= 1 - 1 := by rw[h]
+    _= 0 := by norm_num
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero g with h₁ | h₂
+  left
+  have : x = 1 := by
+    calc
+    x = x + 0 := by rw[add_zero]
+    _= x + (1 - 1) := by rw[sub_self]
+    _= x + (1 + -1) := by rw[sub_eq_add_neg]
+    _= x + (-1 + 1) := by nth_rw 2 [add_comm]
+    _= (x + -1) + 1 := by rw[← add_assoc]
+    _= (x - 1) + 1 := by rw[← sub_eq_add_neg]
+    _= 0 + 1 := by rw[h₁]
+    _= 1 := by rw[zero_add]
+  assumption
+  right
+  have : x = -1 := by
+    calc
+    x = x + 0 := by rw[add_zero]
+    _= x + (1 - 1) := by rw[sub_self]
+    _= x + (1 + -1) := by rw[sub_eq_add_neg]
+    _= (x + 1) + -1 := by rw[← add_assoc]
+    _= 0 + -1 := by rw[h₂]
+    _= - 1 := by rw[zero_add]
+  assumption
 
 example (h : x ^ 2 = y ^ 2) : x = y ∨ x = -y := by
-  sorry
+  have g : (x-y)*(x+y) = 0 := by
+    calc
+    (x-y)*(x+y) = x^2 - y^2 := by ring
+    _= x^2 - x^2 := by rw[h]
+    _= 0 := by apply sub_self
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero g with h₁ | h₂
+  left
+  have : x = y := by
+    calc
+    x = x + 0 := by rw[add_zero]
+    _= x + (y - y) := by rw[sub_self]
+    _= x + (y + -y) := by rw[sub_eq_add_neg]
+    _= x + (-y + y) := by nth_rw 2 [add_comm]
+    _= (x + -y) + y := by rw[← add_assoc]
+    _= (x - y) + y := by rw[← sub_eq_add_neg]
+    _= 0 + y := by rw[h₁]
+    _= y := by rw[zero_add]
+  assumption
+  right
+  have : x = -y := by
+    calc
+    x = x + 0 := by rw[add_zero]
+    _= x + (y - y) := by rw[sub_self]
+    _= x + (y + -y) := by rw[sub_eq_add_neg]
+    _= (x + y) + -y := by rw[← add_assoc]
+    _= 0 + -y := by rw[h₂]
+    _= - y := by rw[zero_add]
+  assumption
 
 end
 
@@ -150,4 +249,21 @@ example (P : Prop) : ¬¬P → P := by
   contradiction
 
 example (P Q : Prop) : P → Q ↔ ¬P ∨ Q := by
-  sorry
+  constructor
+  intro h
+  by_cases h' : P
+  right
+  apply h h'
+  left
+  assumption
+  cases em P
+  intro h₁
+  rcases h₁ with np | q
+  contradiction
+  intro h₂
+  assumption
+  intro h₃
+  intro h₄
+  rcases h₃ with np | q
+  contradiction
+  assumption
